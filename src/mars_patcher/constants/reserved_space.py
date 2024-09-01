5835f8de60bf6b2656cae92f72adee1fc0eb8be9
Miepee <janbidler00@protonmail.com>
2024-09-01 20:12:54 +0200

    docstring

--- src/mars_patcher/constants/reserved_space.py
+++ src/mars_patcher/constants/reserved_space.py
@@ -1,14 +1,11 @@
-# Reserved Space is space in the rom that the base patch specifically has reserved for easy patcher modifications.
-
 class ReservedConstants:
+    """These are constants that are in the patches 'Reserced Space'; things that are intended to be modified by this patcher."""
     # These need to be kept in sync with the base patch
     # found somewhere around https://github.com/MetroidAdvRandomizerSystem/MARS-Fusion/blob/main/src/main.s#L45
     MINOR_LOCS_TABLE_ADDR = 0x7FF000
     MAJOR_LOCS_ADDR = 0x7FF01C
     TANK_INC_ADDR = 0x7FF046
-
-    # TODO: patch uses this variable for showing messages boxes more nicely. implement this.
-    TOTAL_METROID_COUNT_ADDR = 0x7FF04C
+    TOTAL_METROID_COUNT_ADDR = 0x7FF04C  # TODO: patch uses this variable for showing messages boxes more nicely, implement this
     REQUIRED_METROID_COUNT_ADDR = 0x7FF04D
     STARTING_LOCATION_ADDR = 0x7FF04E
     CREDITS_END_DELAY_ADDR = 0x7FF056   # TODO: is this meant to be changed?
