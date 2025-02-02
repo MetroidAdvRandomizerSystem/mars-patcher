368438f22a755be788a02d4b720c3cf8e1952518
zarakava <zarakava@zarakorner.com>
2025-02-02 14:42:13 -0600

    Prevent duplicate rooms from being set

--- src/mars_patcher/room_names.py
+++ src/mars_patcher/room_names.py
@@ -1,4 +1,4 @@
-from mars_patcher.auto_generated_types import MarsschemaRoomnamesItem
+from mars_patcher.auto_generated_types import Areaid, MarsschemaRoomnamesItem, Typeu8
 from mars_patcher.constants.reserved_space import ReservedConstants
 from mars_patcher.rom import Rom
 from mars_patcher.text import MessageType, encode_text
@@ -12,10 +12,15 @@ ROOM_NAMES_TABLE_ADDR = ReservedConstants.ROOM_NAMES_TABLE_ADDR
 # - Area Room names are indexed by room id. This means some entries
 #   are never used, but this allows for easy lookup
 def write_room_names(rom: Rom, data: list[MarsschemaRoomnamesItem]) -> None:
+    seen_rooms: set[tuple[Areaid, Typeu8]] = set()
     for room_name_entry in data:
         area_id = room_name_entry["Area"]
         room_id = room_name_entry["Room"]
         room_name = room_name_entry["Name"]
+
+        # Check that the room wasn't already set
+        assert (area_id, room_id) not in seen_rooms, "Duplicate room name provided."
+        seen_rooms.add((area_id, room_id))
 
         # Find room name table by indexing at ROOM_NAMES_TABLE_ADDR
         area_addr = ROOM_NAMES_TABLE_ADDR + (area_id * 4)
